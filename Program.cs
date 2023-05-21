1571e93e6cdcb346e743353cdfbb89e5f3d1b450
pavel <logumanov.pavel@gmail.com>
2023-05-21 13:50:21 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,45 +1,45 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// Console.Write("input number A: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.Write("input number B: ");
-// int b = Convert.ToInt32(Console.ReadLine());
-// int stepenAotB (int a)
-// {
-//     int x = a;
-//     for (int i = 0; i < b - 1; i++)
-//     {
-//         x = x*a;
-//     }
-//     return x;
-// }
-// Console.Write ($"степень числа А от В -> {stepenAotB(a)}");
+Console.Write("input number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("input number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int stepenAotB (int a)
+{
+    int x = a;
+    for (int i = 0; i < b - 1; i++)
+    {
+        x = x*a;
+    }
+    return x;
+}
+Console.Write ($"степень числа А от В -> {stepenAotB(a)}");
 
 
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// Console.Write("Введите число:");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int len = Length(a);
-// Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
-// int Length(int a)
-// {
-// int index = 0;
-// while (a > 0)
-// {
-// a /= 10;
-// index++;
-// }
-// return index;
-// }
-// int Sum(int a, int len)
-// {
-// int sum = 0;
-// for (int i = 1; i <= len; i++)
-// {
-// sum += a % 10;
-// a /= 10;
-// }
-// return sum;
-// }
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int len = Length(a);
+Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
+int Length(int a)
+{
+int index = 0;
+while (a > 0)
+{
+a /= 10;
+index++;
+}
+return index;
+}
+int Sum(int a, int len)
+{
+int sum = 0;
+for (int i = 1; i <= len; i++)
+{
+sum += a % 10;
+a /= 10;
+}
+return sum;
+}
 
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
